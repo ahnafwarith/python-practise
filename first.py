6fe540185e10ab8d1fb9c855fbed6e136cc7c2b8
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-14 07:24:30 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,16 @@
+# Functions
+def addTwo(x, y):
+    sum = x + y
+    return sum
+
+
+add = addTwo(10, 20)
+print(add)
+
+# Lists |arrays in JS|
+marks = [90, 95, 100]
+print(marks[-1])  # using negative indexing
+
+# Dictionary  |objects in JS|
+marksAgain = {"english": 95, "CSE": 100}
+print(marksAgain["english"])
